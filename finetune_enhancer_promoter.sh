8f1e39c1184a833221df79034cc07036cbba58a9
Jocelyn Song <zqsong108@gmail.com>
2024-06-14 00:15:58 -0400

    Update finetune_enhancer_promoter.sh

--- finetune_enhancer_promoter.sh
+++ finetune_enhancer_promoter.sh
@@ -4,5 +4,5 @@
 #export HYDRA_FULL_ERROR=1
 
 python -m train wandb=null experiment=hg38/enhancer_promoter_benchmark \
-train.ckpt=pretrained_models/weights.ckpt \
+train.pretrained_model_path=pretrained_models/weights.ckpt \
 dataset.batch_size=1
